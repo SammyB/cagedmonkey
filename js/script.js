ce9898eb3362d6d1afc6b74e6a93b1a399f57c5f
visualjazz-sbrunno <sbrunno@visualjazz.com.au>
2014-08-09 13:16:55 +1000

    coming soon website done.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('.cover').hide(0).delay(200).fadeIn(1000);
+  $('.logo').hide(0).delay(500).fadeIn(1000);
+  $('.left').hide(0).delay(200).fadeIn(1000);
+  $('.right').hide(0).delay(200).fadeIn(1000);
+  $('.home').hide(0).delay(1500).fadeIn(1000);
+});
